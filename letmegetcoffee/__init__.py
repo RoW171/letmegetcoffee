0387e51f1b727e0593f3c23fda122286624fd1f6
r0w171 <robinweiland@gmx.de>
2019-04-02 03:23:32 +0200

    some testing and a couple of docstings

--- letmegetcoffee/__init__.py
+++ letmegetcoffee/__init__.py
@@ -22,21 +22,40 @@ from time import sleep
 
 
 class lmgc:
+    """
+
+    """
+
     EXCEPTIONS = (Exception,)
     ON_EXCEPTION = print
 
     @staticmethod
     def catch(func):
-        print(lmgc.ON_EXCEPTION)
+        """
+        decorator for the function that should be monitored
+        like this:
+
+        @lmgc.catch
+        def randomFunction(arg):
+            ...
+
+        :param func: the function; gets handled by the decorator
+        :return: the wrapper; also part of the decorator
+        """
         if lmgc.ON_EXCEPTION == print: warn('lmgc.ON_EXCEPTION is not specified; defaults to print')
 
-        def wrapper():
-            try: func()
+        def wrapper(*args, **kwargs):
+            try: func(*args, **kwargs)
             except lmgc.EXCEPTIONS as e: lmgc.ON_EXCEPTION(e)
         return wrapper
 
     @staticmethod
     def on_exception_beep(e):
+        """
+        beep when an error occurred, not to be called by the user, only set onto lmgc.ON_EXCEPTION
+        :param e: Exception(child) object; passed by lmgc.catch
+        :return: None
+        """
         name = e.__class__.__name__
         line = e.__traceback__.tb_lineno
         file = e.__traceback__.tb_frame.f_code.co_filename
@@ -52,10 +71,13 @@ class lmgc:
                 sleep(0.5)
 
 
+lmgc.ON_EXCEPTION = lmgc.on_exception_beep
+
+
 @lmgc.catch
-def testing():
+def testing(number):
     from random import randint
-    number = 1000000
+    # number = 1000000
     counter = int()
     while True:
         counter += 1
@@ -64,4 +86,4 @@ def testing():
 
 
 if __name__ == '__main__':
-    testing()
+    testing(20)
